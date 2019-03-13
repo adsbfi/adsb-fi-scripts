3fdaf78c099b47a2a77411e719a1cdca80ad6181
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2019-03-13 13:44:45 -0700

    apt install debhelper
    debhelper dependency for Jessie isntalls

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ if [ $(dpkg-query -W -f='${STATUS}' curl 2>/dev/null | grep -c "ok installed") -
     echo "Installing the curl package..."
     echo -e "\033[37m"
     sudo apt-get update
-    sudo apt-get install -y curl
+    sudo apt-get install -y curl debhelper
 fi
 echo -e "\033[37m"
 
