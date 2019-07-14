b7d18956939d32b42cd058a354c67d4d23b02456
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2019-07-13 20:40:45 -0700

    variable name change

--- setup.sh
+++ setup.sh
@@ -167,14 +167,14 @@ fi
     echo "------------------------------------------------------" >> $LOGFILE
     echo "" >> $LOGFILE
     
-    ADSBEXCHANGEUSERNAME="$(echo -e "${ADSBEXCHANGEUSERNAME}" | tr -d '[:space:]')"
+    NOSPACENAME="$(echo -e "${ADSBEXCHANGEUSERNAME}" | tr -d '[:space:]')"
     # Create the mlat-client maintenance script.
     tee adsbexchange-mlat_maint.sh > /dev/null <<EOF
 #!/bin/sh
 while true
   do
     sleep 30
-    /usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $RECEIVERLATITUDE --lon $RECEIVERLONGITUDE --alt $RECEIVERALTITUDE --user $ADSBEXCHANGEUSERNAME --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
+    /usr/bin/mlat-client --input-type dump1090 --input-connect localhost:30005 --lat $RECEIVERLATITUDE --lon $RECEIVERLONGITUDE --alt $RECEIVERALTITUDE --user $NOSPACENAME --server feed.adsbexchange.com:31090 --no-udp --results beast,connect,localhost:30104
   done
 EOF
 
