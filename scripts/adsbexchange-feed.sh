80f6b73d8eaea2d525e365141c72259467a62a86
Matthias Wirth <matthias.wirth@gmail.com>
2019-07-25 00:26:07 +0200

    Change netcat-maint script to be started via a systemd service
    also change the scripts name

---
+++ scripts/adsbexchange-feed.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+while sleep 30
+do
+	if ping -q -c 2 -W 5 feed.adsbexchange.com >/dev/null 2>&1
+	then
+		echo Connected to feed.adsbexchange.com:$RECEIVERPORT
+		/usr/bin/socat -u TCP:localhost:30005 TCP:feed.adsbexchange.com:$RECEIVERPORT
+		echo Disconnected
+	else
+		echo Unable to connect to feed.adsbexchange.com, trying again in 30 seconds!
+	fi
+done
