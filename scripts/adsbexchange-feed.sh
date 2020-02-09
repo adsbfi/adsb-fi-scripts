baadfc086b94abeb77791e60be2203c6ad05cb4b
Matthias Wirth <matthias.wirth@gmail.com>
2020-02-09 15:29:19 +0100

    minor socat feed script changes

--- scripts/adsbexchange-feed.sh
+++ scripts/adsbexchange-feed.sh
@@ -3,14 +3,15 @@
 trap "kill 0" SIGINT
 trap "kill -2 0" SIGTERM
 
-while sleep 30
+while sleep 5
 do
 	if ping -q -c 2 -W 5 feed.adsbexchange.com >/dev/null 2>&1
 	then
 		echo Connected to feed.adsbexchange.com:$SERVERPORT
 		/usr/bin/socat -u TCP:$INPUT TCP:feed.adsbexchange.com:$SERVERPORT
-		echo Disconnected
+		echo Disconnected, reconnecting in 30 seconds!
 	else
 		echo Unable to connect to feed.adsbexchange.com, trying again in 30 seconds!
 	fi
+    sleep 25
 done
