d757de503c5a0629b361148b5974092a698204fe
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2020-02-11 23:54:51 -0700

    added feed-adsbx

--- scripts/adsbexchange-feed.sh
+++ scripts/adsbexchange-feed.sh
@@ -3,13 +3,22 @@
 trap "kill 0" SIGINT
 trap "kill -2 0" SIGTERM
 
+SOURCE="--net-connector localhost,30005,beast_in"
+TARGET="--net-connector feed.adsbexchange.com,"$SERVERPORT",beast_reduce_out"
+NET_OPTIONS="--net-only --net --net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 0 --net-bo-port 0 --net-ri-port 0"
+
 while sleep 5
 do
 	if ping -q -c 2 -W 5 feed.adsbexchange.com >/dev/null 2>&1
 	then
 		echo Connected to feed.adsbexchange.com:$SERVERPORT
-		/usr/local/bin/feed-adsbx
+		
+		/usr/local/share/feed-adsbx --net --net-only --quiet \
+		--net-beast-reduce-interval 0.5 \
+		$SOURCE $TARGET $NET_OPTIONS
+		
 		#/usr/bin/socat -u TCP:$INPUT TCP:feed.adsbexchange.com:$SERVERPORT
+		
 		echo Disconnected, reconnecting in 30 seconds!
 	else
 		echo Unable to connect to feed.adsbexchange.com, trying again in 30 seconds!
