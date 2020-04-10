16d83c5a4fdc696b1fca5187e4d9568b3c39d310
Matthias Wirth <matthias.wirth@gmail.com>
2020-04-10 20:44:25 +0200

    solve SEGV issue with feed script.

--- scripts/adsbexchange-feed.sh
+++ scripts/adsbexchange-feed.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-trap "kill 0" INT
-trap "kill -2 0" TERM
-
 INPUT_IP=$(echo $INPUT | cut -d: -f1)
 INPUT_PORT=$(echo $INPUT | cut -d: -f2)
 SOURCE="--net-connector $INPUT_IP,$INPUT_PORT,beast_in"
