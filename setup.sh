70f09c748adfe1bb3b535c795e954f946de0464e
Matthias Wirth <matthias.wirth@gmail.com>
2020-04-11 19:04:28 +0200

    add some explanation to /etc/default/adsbexchange

--- setup.sh
+++ setup.sh
@@ -284,13 +284,19 @@ fi
     tee /etc/default/adsbexchange > /dev/null <<EOF
     INPUT="127.0.0.1:30005"
     REDUCE_INTERVAL="0.5"
+
+    # feed name for checking MLAT sync (adsbx.org/sync)
     USER="${NOSPACENAME}_$((RANDOM % 90 + 10))"
+
     RECEIVERLATITUDE="$RECEIVERLATITUDE"
     RECEIVERLONGITUDE="$RECEIVERLONGITUDE"
+
     RECEIVERALTITUDE="$RECEIVERALTITUDE"
+
     RESULTS="--results beast,connect,localhost:30104 --results basestation,listen,31003"
-    MLATSERVER="feed.adsbexchange.com:31090"
     INPUT_TYPE="dump1090"
+
+    MLATSERVER="feed.adsbexchange.com:31090"
     TARGET="--net-connector feed.adsbexchange.com,30005,beast_reduce_out"
     NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 0 --net-bo-port 0 --net-ri-port 0"
 EOF
