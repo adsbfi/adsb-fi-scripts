832c1a950fa18a163296f7be3468d18a93ca521a
Matthias Wirth <matthias.wirth@gmail.com>
2021-07-01 11:02:15 +0200

    remove optional tar1090 instance when uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,10 @@ systemctl disable --now adsbexchange-mlat
 systemctl disable --now adsbexchange-mlat2 &>/dev/null
 systemctl disable --now adsbexchange-feed
 
+if [[ -d /usr/local/share/tar1090/html-adsbx ]]; then
+    bash /usr/local/share/tar1090/uninstall.sh adsbx
+fi
+
 rm -f /lib/systemd/system/adsbexchange-mlat.service
 rm -f /lib/systemd/system/adsbexchange-mlat2.service
 rm -f /lib/systemd/system/adsbexchange-feed.service
