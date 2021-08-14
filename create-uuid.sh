96050bdd1d1c5a3ae66ba16dc771b94678e262a9
Matthias Wirth <matthias.wirth@gmail.com>
2021-08-14 13:29:54 +0200

    cleanup create-uuid

--- create-uuid.sh
+++ create-uuid.sh
@@ -3,20 +3,26 @@
 UUID_FILE="/boot/adsbx-uuid"
 
 # Let's make sure the UUID tools are installed...
-UUIDGEN=$(command -v uuidgen)
-RV=$?
 
-if [ $RV -ne 0 ]; then
-    echo "Can't find uuidgen in path, trying to install uuidgen..."
-    apt-get -y install uuid-runtime
-    UUIDGEN=$(command -v uuidgen)
-    RV=$?
-    if [ $RV -ne 0 ]; then
-        echo "Failed to install uuid-runtime package - need manual intervention!"
-        sleep 60
-        exit 10
+
+function generateUUID() {
+    if ! command -v uuidgen &>/dev/null; then
+        echo "Can't find uuidgen in path, trying to install uuidgen..."
+        apt update
+        apt install -y --no-install-suggests --no-install-recommends uuid-runtime
+        if ! command -v uuidgen &>/dev/null; then
+            echo "Failed to install uuid-runtime package - need manual intervention!"
+            sleep 60
+            exit 10
+        fi
     fi
-fi
+
+    rm -f $UUID_FILE
+    sleep 0.$RANDOM; sleep 0.$RANDOM
+    UUID=$(uuidgen)
+    echo New UUID: $UUID
+    echo $UUID > $UUID_FILE
+}
 
 # Check for a (valid) UUID...
 if [ -f $UUID_FILE ]; then
@@ -24,19 +30,14 @@ if [ -f $UUID_FILE ]; then
     if ! [[ $UUID =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
         # Data in UUID file is invalid.  Regenerate it!
         echo "WARNING: Data in UUID file was invalid.  Regenerating UUID."
-        rm -f $UUID_FILE
-        UUID=$($UUIDGEN)
-        echo New UUID: $UUID
-        echo $UUID > $UUID_FILE
+        generateUUID
     else
         echo "Using existing valid UUID ($UUID) from $UUID_FILE"
     fi
 else
     # not found generate uuid and save it
     echo "WARNING: No UUID file found, generating new UUID..."
-    UUID=$($UUIDGEN)
-    echo New UUID: $UUID
-    echo $UUID > $UUID_FILE
+    generateUUID
 fi
 
 exit 0
