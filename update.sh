5dfb87cabbcfc2cf52a963f57608742064fcfbf6
tmantti <tmantti@users.noreply.github.com>
2025-08-29 21:50:50 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -220,10 +220,14 @@ else
     if /usr/bin/python3 -m venv $VENV >> $LOGFILE \
         && echo 36 \
         && source $VENV/bin/activate >> $LOGFILE \
+        && echo 37 \
+        && python3 -c "import setuptools" || python3 -m pip install setuptools >> $LOGFILE \
         && echo 38 \
-        && python3 -m pip install --upgrade pip >> $LOGFILE \
+        && python3 -c "import asyncore" || python3 -m pip install pyasyncore >> $LOGFILE \
+        && echo 39 \
+        && python3 setup.py build >> $LOGFILE \
         && echo 40 \
-        && python3 -m pip install . >> $LOGFILE \
+        && python3 setup.py install >> $LOGFILE \
         && echo 46 \
         && revision > $IPATH/mlat_version || rm -f $IPATH/mlat_version \
         && echo 48 \
